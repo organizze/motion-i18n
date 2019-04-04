8a631122f57298eb3f84810b4d5e617ca0fcc516
Felipe <solanoluz@gmail.com>
2019-04-04 16:04:23 -0300

    added Custom Locale Manager support

--- lib/motion-i18n-ios/i18n.rb
+++ lib/motion-i18n-ios/i18n.rb
@@ -1,16 +1,53 @@
 module I18n
   class << self
+
     def translate(key, substitutions = {})
-      String.new((NSBundle.mainBundle.localizedStringForKey(key, value:"", table:nil))).tap do |result|
+      String.new((bundle.localizedStringForKey(key, value:"", table:nil))).tap do |result|
         substitutions.each do |key, value|
           result.gsub!("%{#{key}}", value.to_s)
         end
       end
     end
+
     alias t translate
 
-    def locale
-      NSLocale.preferredLanguages.first
+    def availableLanguages
+      @availableLanguages || []
+    end
+
+    def setAvailableLanguages(languages)
+      @availableLanguages = languages
+    end
+
+    def setLanguage(languageLocale)
+      NSUserDefaults.standardUserDefaults.setObject(languageLocale, forKey: nsUserDefaultsKey)
+      NSUserDefaults.standardUserDefaults.synchronize
+      @language = languageLocale
+    end
+
+    def language
+      @language ||= begin
+        NSUserDefaults.standardUserDefaults.objectForKey(nsUserDefaultsKey) || defaultLanguage
+      end
+    end
+
+    def bundle
+      NSBundle.bundleWithPath(bundlePath)
+    end
+
+    def bundlePath
+      NSBundle.mainBundle.pathForResource(language, ofType: "lproj") || NSBundle.mainBundle.pathForResource(defaultLanguage, ofType: "lproj")
+    end
+
+    def defaultLanguage
+      foundLang = NSLocale.preferredLanguages.find do |l|
+        availableLanguages.include?(l)
+      end
+      foundLang || 'en'
+    end
+
+    def nsUserDefaultsKey
+      "motion-i18n_locale"
     end
   end
 end
